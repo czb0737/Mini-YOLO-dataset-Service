66e31073205f61393b62235e264a6fa83764f971
czb0737 <czb0737@gmail.com>
2025-11-25 17:00:14 +0800

    Delete unused files

--- deploy/sae-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd backend
-docker build -t registry.cn-guangzhou.aliyuncs.com/your-namespace/backend:latest .
-docker push registry.cn-guangzhou.aliyuncs.com/your-namespace/backend:latest
